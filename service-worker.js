157eefc2fa856e6ee9dc08fe416aa2c5d0d2b6dc
jgroc-de <jgroc-de@student.42.fr>
2020-02-16 00:59:10 +0100

    update install

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 'use strict'
 
 // Update cache names any time any of the cached files change.
-const VERSION = '0.5.2'
+const VERSION = '0.6.1'
 const CACHE_NAME = 'static-cache-v' + VERSION
 
 // Add list of files to cache here.
