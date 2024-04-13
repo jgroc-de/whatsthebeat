8aa01b5c74a6815d755e9545e38c3f501cab578b
jgroc-de <jerome.gds@gmail.com>
2024-04-13 02:46:54 +0200

    update node

--- www/index.php
+++ www/index.php
@@ -9,14 +9,17 @@ $twig = new \Twig\Environment($loader, [
 ]);
 
 switch ($_SERVER['REQUEST_URI']) {
+    case '/tuner.html':
+        echo $twig->render('tap-the-beat.html.twig', ['current_page' => 'tuner']);
+        return true;
     case '/tap-the-beat.html':
-        echo $twig->render('tap-the-beat.html.twig');
+        echo $twig->render('tap-the-beat.html.twig', ['current_page' => 'tap']);
         return true;
     case '/':
-        echo $twig->render('index.html.twig');
+        echo $twig->render('index.html.twig', ['current_page' => 'about']);
         return true;
     case '/settings.html':
-        echo $twig->render('settings.html.twig');
+        echo $twig->render('settings.html.twig', ['current_page' => 'settings']);
         return true;
     default:
         return false;
