c53dda42b39893f2d5e7221218051f64c76ab9d4
jgroc-de <jerome.gds@gmail.com>
2024-04-15 02:57:26 +0200

    update node

--- www/index.php
+++ www/index.php
@@ -19,7 +19,7 @@ switch ($_SERVER['REQUEST_URI']) {
         echo $twig->render('metronome.html.twig', ['current_page' => 'metronome']);
         return true;
     case '/':
-        echo $twig->render('index.html.twig', ['current_page' => 'about']);
+        echo $twig->render('about.html.twig', ['current_page' => 'about']);
         return true;
     case '/settings.html':
         echo $twig->render('settings.html.twig', ['current_page' => 'settings']);
